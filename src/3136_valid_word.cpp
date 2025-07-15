ee2b9f4c671d0b0f9ed2773238e4a24e74f5dcc4
Christian Valenti <christian.valenti97@gmail.com>
2025-07-15 14:51:36 +0200

    First code :computer:

---
+++ src/3136_valid_word.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.length() < 3) {
+            return false;
+        }
+        bool has_vowel = false;
+        bool has_consonant = false;
+
+        for (char c : word) {
+            if (!std::isalpha(c) && !std::isdigit(c)) {
+                return false;
+            }
+            has_vowel |= is_vowel(c);
+            has_consonant |= is_consonant(c);
+        }
+
+        return has_vowel && has_consonant;
+    }
+
+    bool is_vowel(char c) {
+        c = std::tolower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    bool is_consonant(char c) {
+        c = std::tolower(c);
+        return !is_vowel(c) && 'a' <= c && c <= 'z';
+    }
+};
